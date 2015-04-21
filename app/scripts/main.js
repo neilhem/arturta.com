f1c7342a173caffad595f2387c53bab9816f6336
Rakhat Jabagin <neilhem@hotmail.com>
2015-04-21 23:52:50 +0600

    update markup

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,17 +10,35 @@ $(function() {
     }
   }
 
-  var mySwiper = new Swiper ('.js-carousel-main', {
+  var mainSlider = new Swiper('.js-carousel-main', {
     pagination: '.swiper-pagination',
     paginationClickable: true,
     direction: 'vertical',
     mousewheelControl: true,
-    mousewheelForceToAxis: true,
     onInit: function(swiper) {
     	setBodyClass(swiper.slides[swiper.activeIndex].dataset.theme);
     },
     onSlideChangeStart: function(swiper) {
     	setBodyClass(swiper.slides[swiper.activeIndex].dataset.theme);
+    }
+  });
+
+  var teamSlider = new Swiper('.js-carousel-team', {
+    pagination: '.team-pagination',
+    paginationClickable: true,
+    bulletClass: 'team-pagination-item',
+    bulletActiveClass: 'team-pagination-item-active',
+    direction: 'vertical',
+    mousewheelControl: true,
+    onInit: function(swiper) {
+      setBodyClass(swiper.slides[swiper.activeIndex].dataset.theme);
+    },
+    onSlideChangeStart: function(swiper) {
+      setBodyClass(swiper.slides[swiper.activeIndex].dataset.theme);
+    },
+    paginationBulletRender: function(index, className) {
+      var data = $('.js-carousel-team .swiper-slide').eq(index).data();
+      return '<li class="' + className + '"><span>' + data.name + '</span> ' + data.position + '</li>';
     }
   });
  });
